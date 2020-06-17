6fbca56d1186f8e20210bb8bafc7aaf137384ee1
Schmid Payen <schmid@magehire.com>
2020-06-17 14:04:33 -0400

    Landing page layout(header,contentpart,footer)and registe&login view complete,started about_us view

--- application/views/website/pages/about_us.php
+++ application/views/website/pages/about_us.php
@@ -1,11 +1,11 @@
  <!--==========================
     Intro Section
   ============================-->
-  <section id="intro" style="background-image: url('<?php echo base_url()?>/assets/img/intro-carousel/1.jpg')">
+  <section id="intro" class="bg-primary" style="height:40vh">
 
-
+    <h1 styel="position:absolute;top:0;bottom:0">About Us</h1>
   </section><!-- #intro -->
 
-  <main id="main">
-
-  </main>
+    <main id="main">
+    <h1>heloo</h1>
+    </main>
